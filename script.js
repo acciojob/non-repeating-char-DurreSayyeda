ca89ceb90caa03a8104df2015ea66b82ff7d70bc
Sayyeda Afshan <syed1995.sa@gmail.com>
2023-12-11 23:27:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-	const char = {}
-	 for (const char of str) {
-        charCount[char] = (charCount[char] || 0) + 1;
-	 }
+	const charCounts = new Map();
+for (const char of str) {
+charCounts.set(char, (charCounts.get(char) || 0) + 1);
+}
+
+for (const char of str) {
+if (charCounts.get(char) === 1) {
+return char;
+}
+}
+return null;
  }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
